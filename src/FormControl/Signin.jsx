d5d1fff6e9b81cdfb2177f19ec9fe4750fe44385
abdulsami13 <155838700+abdulsami13@users.noreply.github.com>
2024-08-02 00:03:22 +0500

    changes 3 time
    Changes 3 time again

---
+++ src/FormControl/Signin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Signin() {
+  return (
+    <div>
+      
+
+<p>Testing two </p>
+      <input type="text"  />
+
+      <button>sumit here </button>
+    </div>
+  )
+}
